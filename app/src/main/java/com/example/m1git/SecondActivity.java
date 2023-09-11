c2ed500384a400c6d2dbd44e1ef3d323b3afe77d
IslombekYadgarov <islam67747@gmail.com>
2023-09-11 15:49:31 +0500

    second commit

--- app/src/main/java/com/example/m1git/SecondActivity.java
+++ app/src/main/java/com/example/m1git/SecondActivity.java
@@ -4,14 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-      //create project
-        //secondActivity ochildi
+        setContentView(R.layout.activity_second);
+        //bu second
     }
 }
